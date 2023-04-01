9efd32bf0b2780fa107d78e84a349155a730a30b
Milner Chen <milnerchen0@gmail.com>
2023-04-01 18:31:38 -0400

    store setup

---
+++ frontend/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import configureStore from './store';
+
+const store = configureStore();
+
+if (process.env.NODE_DEV !== 'production') {
+  window.store = store;
+}
+
+const Root = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
+);

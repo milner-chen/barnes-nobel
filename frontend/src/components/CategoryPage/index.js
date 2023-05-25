90a504bb576bdb85e2d477675010c93c993c5828
Milner Chen <milnerchen0@gmail.com>
2023-05-25 14:41:44 -0400

    fix rendering 404 page logic

--- frontend/src/components/CategoryPage/index.js
+++ frontend/src/components/CategoryPage/index.js
@@ -6,6 +6,7 @@ import { useHistory, useParams } from "react-router-dom";
 import CategoryPageItem from "../CategoryPageItem";
 import * as wishlistActions from "../../store/wishlist";
 import './CategoryPage.css'
+import NotFoundPage from "../NotFoundPage";
 
 const CategoryPage = () => {
     const { category } = useParams();
@@ -27,22 +28,27 @@ const CategoryPage = () => {
         // fetchFunc();
     }, [user?.id])
     
-    if (!isCategory && products?.length) {
-        // console.log(isCategory);
-        // console.log(products);
-        history.push('/404')
-    };
+    // if (!isCategory && products?.length) {
+    //     // console.log(isCategory);
+    //     // console.log(products);
+    //     history.push('/404')
+    // };
+
     if (!products) return null;
 
     return (
-        <div className="cat-page">
+        <>
+        {isCategory ? (<div className="cat-page">
             <div className="cat-body">
                 <ul className="cat-content">
                     <h1 className="cat-header">{category}</h1>
                     {products.map(product => <CategoryPageItem key={product?.id} product={product} user={user?.id} />)}
                 </ul>
             </div>
-        </div>
+        </div>) : (
+            <NotFoundPage />
+        )}
+        </>
     )
 }
 
